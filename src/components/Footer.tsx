d08ac99eab15e975f0a31b7dd95c3e8fa9442093
Arisch24 <Arischvaran00@gmail.com>
2023-08-02 22:52:07 +0800

    Remove boilerplate

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="socials-container">
+                
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+import './Header.scss';
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Me</a></li>
+                    <li><a href="">Projects</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
